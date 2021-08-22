0542c661054c42a199c1125a6230fa7b28665675
MacLoki <newbeeloki@gmail.com>
2021-08-22 13:11:44 +0800

    Deleted the irrelevant files

--- node-test/http.js
+++ node-test/http.js
@@ -88,21 +88,21 @@ const sendPostRequest = () => {
 
 // * 3. 发送 HTTP POST 请求
 // ? 使用 Axios 库
-const axios = require('axios')
+// const axios = require('axios')
 
-axios
-  .post('http://nodejs.cn/todos', {
-    todo: 'do something'
-  })
-  .then(res => {
-    console.log(`Status code: ${res.statusCode}`)
-  })
-  .catch(err => console.error(err))
+// axios
+//   .post('http://nodejs.cn/todos', {
+//     todo: 'do something'
+//   })
+//   .then(res => {
+//     console.log(`Status code: ${res.statusCode}`)
+//   })
+//   .catch(err => console.error(err))
 
 
 // * 4. 获取 HTTP 请求的正文数据
 // 请求正文中提取以 JSON 格式发送的数据
-// ? Send request by Axios
+// ? Send request by Axios and Express
 const axios = require('axios')
 
 axios.post('http://nodejs.cn/todos', {
@@ -113,4 +113,41 @@ axios.post('http://nodejs.cn/todos', {
 const express =require('express')
 const app = express()
 
-app.use()
+// ! 使用 Express 抽象了许多 Node.js 中相应的操作
+// 用法类似 app.get() 或 app.post()
+// app.get/post(path, callback) 中的 callback 只能是函数
+// ! app.use(path, callback) 中的 callback 既可以是 router 对象又可以是函数 
+// app.use(
+//   express.urlencoded({
+//     extended: true
+//   })
+// )
+
+// app.use(express.json())
+
+// app.post('/todos', (req, res) => {
+//   console.log(req.body.todo)
+// })
+
+// ? Send request by Nodejs builtin modules
+// const server = http.createServer((req, res) => {
+//   // 监听流的 data 事件获取数据
+//   req.on('data', chunk => {
+//     console.log(`Available data chunk: ${chunk}`)
+//   })
+//   req.on('end', () => {
+//     // 数据结束
+//     console.log('The data transmission has completed.')
+//   })
+// })
+
+// // 若要访问数据，则必须将其放入数组中
+// const server = http.createServer((req, res) => {
+//   let data = ''
+//   req.on('data', chunk => {
+//     data += chunk
+//   })
+//   req.on('end', () => {
+//     JSON.parse(data).todo  // 'do something'
+//   })
+// })
