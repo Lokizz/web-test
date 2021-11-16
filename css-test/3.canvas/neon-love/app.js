025e0533307fa5cfec6b2895435555c2213f1cb3
MacLoki <newbeeloki@gmail.com>
2021-11-17 07:35:02 +0800

    测试 CSS 中的 gradient 与开始表格的样式测试

--- css-test/3.canvas/neon-love/app.js
+++ css-test/3.canvas/neon-love/app.js
@@ -5,8 +5,18 @@ const canvas = document.getElementById('canvas')
 
 // ? 让 canvas 的尺寸与视窗尺寸保持一致
 // ? 利用 window.innerWidth/innerHeight 获取视窗尺寸
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+let sideLength = window.innerWidth < window.innerHeight ? window.innerWidth / 2 : window.innerHeight / 2
 
-// ? 初始化 GL context
+canvas.width = sideLength
+canvas.height = sideLength
+
+
+// ? 初始化 GL context 对象
 const gl = canvas.getContext('webgl')
+
+// ? 使用 ctx 对象的属性和方法绘制内容
+if(!gl) {
+  console.log("Unable to initialize WebGL")
+}
+
+
